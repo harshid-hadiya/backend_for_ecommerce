47c6404020dc3be924410007b8f27629d048192c
harshid-hadiya <ahirharshidj@gmail.com>
2025-11-17 19:38:30 +0530

    adding features of the descending order fetch

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,20 @@
 const Product = require('../models/Product');
+const { rawListeners } = require('../server');
 
 // Get all products
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const id = req.query?.sort;
+    const products = await Product.find().sort((id === "asc") ? { _id: 1 } : { _id: -1 }); 
     res.status(200).json(products);
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching products', error });
+    res.status(500).json({
+      message: "Error fetching products",
+      error: error.message,
+    });
   }
 };
+
 
 // Create a new product
 exports.createProduct = async (req, res) => {
